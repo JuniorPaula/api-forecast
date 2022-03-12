e3760d8bf0b7617bd5b53d686eb58e34dfcd5b81
Junior Paula <slucivaldo509@gmail.com>
2022-03-12 17:30:50 -0300

    Add BeachModel into mongoose, and apply functional test with validates errors

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,4 +1,7 @@
+import { Beach } from '@src/models/beach';
+
 describe('Beaches functional tests', () => {
+  beforeAll(async () => await Beach.deleteMany({}));
   describe('When create a beach', () => {
     it('Should create a beach with success', async () => {
       const newBeache = {
@@ -14,6 +17,25 @@ describe('Beaches functional tests', () => {
 
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newBeache));
+    });
+
+    it('Should return 422 where there is a validation error', async () => {
+      const newBeache = {
+        lat: 'invalid_string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+
+      const response = await global.testRequest
+        .post('/beaches')
+        .send(newBeache);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
+      });
     });
   });
 });
