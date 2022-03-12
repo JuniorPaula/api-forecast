9e5819941984924a61cf292b85add41b1fcf51f1
Junior Paula <slucivaldo509@gmail.com>
2022-03-12 17:29:14 -0300

    Create BeachesModel to connect database

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,10 +1,22 @@
 import { Controller, Post } from '@overnightjs/core';
+import { Beach } from '@src/models/beach';
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 @Controller('beaches')
 export class BeachesController {
   @Post('')
   public async create(request: Request, response: Response): Promise<void> {
-    response.status(201).send({ ...request.body, id: 'fake-id' });
+    try {
+      const beach = new Beach(request.body);
+      const result = await beach.save();
+      response.status(201).send(result);
+    } catch (err) {
+      if (err instanceof mongoose.Error.ValidationError) {
+        response.status(422).send({ error: err.message });
+      } else {
+        response.status(500).send({ error: 'Internal Server Error.' });
+      }
+    }
   }
 }
