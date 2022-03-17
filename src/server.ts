cc532727013d65b0979e778343a48d60bc1656eb
Junior Paula <slucivaldo509@gmail.com>
2022-03-17 19:30:08 -0300

    Create users model, users controllers and functional test for users

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { ForecastController } from './controllers/forecast';
 import { Application } from 'express-serve-static-core';
 import * as database from '@src/database';
 import { BeachesController } from './controllers/beaches';
+import { UsersController } from './controllers/users';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -24,7 +25,8 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const forecastController = new ForecastController();
     const beachesContoller = new BeachesController();
-    this.addControllers([forecastController, beachesContoller]);
+    const userController = new UsersController();
+    this.addControllers([forecastController, beachesContoller, userController]);
   }
 
   private async databaseSetup(): Promise<void> {
---
+++ test/functional/users.test.ts
@@ -0,0 +1,20 @@
+import { User } from '@src/models/user';
+
+describe('Users functional test', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+  });
+  describe('When create a new user', () => {
+    it('Should successfully create a new user', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+
+      const response = await global.testRequest.post('/users').send(newUser);
+      expect(response.status).toBe(201);
+      expect(response.body).toEqual(expect.objectContaining(newUser));
+    });
+  });
+});
