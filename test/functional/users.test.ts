a09a4cd0a32ac14c7806b41f0a79592b76f3f14a
Junior Paula <slucivaldo509@gmail.com>
2022-03-18 19:57:47 -0300

    Create test to check if the password is hashed

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -1,11 +1,12 @@
 import { User } from '@src/models/user';
+import { AuthService } from '@src/services/auth';
 
 describe('Users functional test', () => {
   beforeEach(async () => {
     await User.deleteMany({});
   });
   describe('When create a new user', () => {
-    it('Should successfully create a new user', async () => {
+    it('Should successfully create a new user with encrypted password', async () => {
       const newUser = {
         name: 'John Doe',
         email: 'john@mail.com',
@@ -14,7 +15,15 @@ describe('Users functional test', () => {
 
       const response = await global.testRequest.post('/users').send(newUser);
       expect(response.status).toBe(201);
-      expect(response.body).toEqual(expect.objectContaining(newUser));
+      await expect(
+        AuthService.comparePassword(newUser.password, response.body.password)
+      ).resolves.toBeTruthy();
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          ...newUser,
+          ...{ password: expect.any(String) },
+        })
+      );
     });
 
     it('Should return 400 when there is a validation error', async () => {
