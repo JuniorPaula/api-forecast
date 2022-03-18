2e543f0a47fb4d05e0bd714e0cb3ba92e9b21dab
Junior Paula <slucivaldo509@gmail.com>
2022-03-17 21:10:44 -0300

    Create baseController to abstract all error of the aplication

---
+++ src/controllers/index.ts
@@ -0,0 +1,32 @@
+import { CUSTOM_VALIDATION } from '@src/models/user';
+import { Response } from 'express';
+import mongoose from 'mongoose';
+
+export abstract class BaseController {
+  protected sendCreateUpdateErrorResponse(
+    response: Response,
+    error: unknown
+  ): void {
+    if (error instanceof mongoose.Error.ValidationError) {
+      const clientErrors = this.hanldeClientErrors(error);
+      response
+        .status(clientErrors.code)
+        .send({ code: clientErrors.code, error: clientErrors.error });
+    } else {
+      response.status(500).send({ code: 500, error: 'Something went wrong.' });
+    }
+  }
+
+  private hanldeClientErrors(error: mongoose.Error.ValidationError): {
+    code: number;
+    error: string;
+  } {
+    const duplicatedKindError = Object.values(error.errors).filter(
+      (err) => err.kind === CUSTOM_VALIDATION.DUPLICATED
+    );
+    if (duplicatedKindError.length) {
+      return { code: 409, error: error.message };
+    }
+    return { code: 422, error: error.message };
+  }
+}
