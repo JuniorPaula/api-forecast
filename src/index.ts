80255eb6602ff1c9ef39350dc994742a1108ae87
Junior Paula <slucivaldo509@gmail.com>
2022-03-22 19:38:16 -0300

    aplly graceful shutdown and treatments errors of the aplication

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,46 @@
 import { SetupServer } from './server';
 import config from 'config';
+import logger from './logger';
+
+enum exitedStatus {
+  Failure = 1,
+  Success = 0,
+}
+
+process.on('unhandledRejection', (reason, promise) => {
+  logger.error(
+    `App exiting due to unhandled promise: ${promise} and reason ${reason}`
+  );
+
+  throw reason;
+});
+
+process.on('uncaughtException', (error) => {
+  logger.error(`App exiting due to uncaught exception: ${error}`);
+  process.exit(exitedStatus.Failure);
+});
 
 (async (): Promise<void> => {
-  const server = new SetupServer(config.get('App.port'));
-  await server.init();
-  server.start();
+  try {
+    const server = new SetupServer(config.get('App.port'));
+    await server.init();
+    server.start();
+
+    const exitedSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+    exitedSignals.map((sig) =>
+      process.on(sig, async () => {
+        try {
+          await server.close();
+          logger.info(`App exited with success!`);
+          process.exit(exitedStatus.Success);
+        } catch (error) {
+          logger.error(`App exited with ${error}`);
+          process.exit(exitedStatus.Failure);
+        }
+      })
+    );
+  } catch (error) {
+    logger.error(`App exited with error ${error}`);
+    process.exit(exitedStatus.Failure);
+  }
 })();
